8e0db99147bbc2f3dd6d7dc99c01d609e0acc953
Bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-05-09 13:53:16 +0200

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+
+import Paper from 'material-ui/Paper'
+
+const styles = {
+  paper: {
+    margin: '1rem',
+    padding: '1rem'
+  }
+}
 
 const PATH = `https://jfddl4-sandbox.firebaseio.com/bartosz/`
 
 class App extends React.Component {
   state = {
     counter: 0,
-    text: ''
+    text: null
   }
 
   componentDidMount() {
@@ -62,18 +72,32 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h2>{this.state.counter}</h2>
-        <button onClick={this.decHandler}>-</button>
-        <button onClick={this.incHandler}>+</button>
-        <br />
-        <TextField
-          hintText="Input text"
-          onChange={(event, value) => this.setState({text: value})}
-          value={this.state.text}
-        />
-        <button onClick={() => this.sendTextToDatabase()}>Send</button>
-      </div>
+      <Paper style={styles.paper}>
+        {this.state.text === null ?
+          'Loading...'
+          :
+          <div >
+            <h2>{this.state.counter}</h2>
+            <button onClick={this.decHandler}>-</button>
+            <button onClick={this.incHandler}>+</button>
+            <br />
+            <TextField
+              fullWidth={true}
+              hintText="Input text"
+              onChange={(event, value) => this.setState({ text: value })}
+              value={this.state.text}
+              />
+
+            <RaisedButton 
+              fullWidth={true}
+              label="Send" 
+              primary={true} 
+              onClick={() => this.sendTextToDatabase()}
+              disabled={ this.state.text === null || this.state.text === '' }
+            />
+          </div>
+        }
+      </Paper>
     )
   }
 }
